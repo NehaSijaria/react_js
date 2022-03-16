df520f5a99c072ecdf11a57a0d02e7d285d253f9
Neha <nehasijaria@gmail.com>
2022-03-16 01:39:11 -0400

    add props via destructing

--- src/AboutBook.jsx
+++ src/AboutBook.jsx
@@ -1,27 +1,34 @@
+import { PromiseProvider } from 'mongoose';
 import React from 'react'
 
   const dynamicTitle = "title1";
 
-const BookName = () => {
+const BookName = (props) => {
+  //props = {title:'', img:''}
+  const {title,img} = props;
+console.log(props);
 
   return (
-    <article className='book'>
-      <Image />
-      <BookTitle />
+    <article className="book">
+      {/* <Image /> */}
+      <img src={img} height="100px" />
+      {/* {props.title} */}
+      <h4>{title}</h4>
     </article>
   );
 }
 const img =
   "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg";
   
-const Image = () => (
-  <img
-    src={img} height="100px"
-    alt=""
-  />
-);
+// const Image = () => (
+//   <img
+//     src={img} height="100px"
+//     alt=""
+//   />
+// );
 //impicit Return //inline styling
 //const BookTitle = () => <h1 style={{color:"red"}}>Book title</h1>;//only expression that return vale , no statement
-const BookTitle = () => <h1>{dynamicTitle}</h1>
+//const BookTitle = () => <h1>{dynamicTitle}</h1>
+
 
 export default BookName
--- src/BookList.jsx
+++ src/BookList.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import AboutBook from "./AboutBook";
 
+
+//if we have 2 books
+const book1 = {
+  title:'title1',
+  img:"https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
+}
+const book2 = {
+  title: "title2",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
+};
+
 const BookList = () => {
   return (
     <section className="bookList">
-      <AboutBook />
-      <AboutBook />
+      {/* <AboutBook title="title1" /> */}
+      <AboutBook img={book1.img} title={book1.title} />
+      <AboutBook img={book2.img} title={book2.title} />
     </section>
   );
 };
